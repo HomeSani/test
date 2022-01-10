f718f53bc1881416719df5584b5979509b930254
HomeSani <52744389+HomeSani@users.noreply.github.com>
2022-01-10 20:22:52 +0300

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1308421.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 64934480, 64934481, 64934473, 64934488, 64934476, 64934471, 64934472, 64934474, 64934484, 64934483, 64934430, 64934432, 64934467, 64934482, 64934485 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
